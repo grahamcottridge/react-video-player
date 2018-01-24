eac4b0bc2e798f2b453240eb1deb9a4a59184e5a
Graham Cottridge <codinggc@gmail.com>
2018-01-24 20:29:43 +0000

    Refactor app function to class component with youtube api search

--- src/index.js
+++ src/index.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = config.BROWSER_KEY_1;
 
 
-// Create a new component.  This component should produce some HTML.
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// Take this component's generated HTML and put it on the page (in the DOM)
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // ES6 would be this.setState({ videos: videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  };
+}
 
 ReactDOM.render(<App />, document.querySelector('.container'));
